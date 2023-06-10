4b3590275ccc27adc44e80048854048d83e1245e
jankefyn <72445405+jankefyn@users.noreply.github.com>
2023-06-10 16:32:50 +0200

    versuch

--- interactive_Audio/script.js
+++ interactive_Audio/script.js
@@ -1,24 +1,109 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Http = require("http");
-const url = require("url");
-const Mongo = require("mongodb");
+/*import * as Http from "http";
+import { ParsedUrlQuery } from "querystring";
+import * as url from "url";
+import * as Mongo from "mongodb";*/
+/*let locationCollection: Mongo.Collection;
+ 
+ 
+
+interface Input {
+  [type: string]: string | string[];
+}
+*/
+/*
+ let databaseUrl: string = "mongodb+srv://FynnJ:nicnjX5MjRSm4wtu@gis-ist-geil.wb5k5.mongodb.net/?retryWrites=true&w=majority";
+ console.log("Starting server");
+ let port: number = Number(process.env.PORT);
+ if (!port)
+   port = 8100;
+ 
+ startServer(port);
+ connectToDatabase(databaseUrl);
+ 
+ 
+ 
+ function startServer(_port: number | string): void {
+   let server: Http.Server = Http.createServer();
+   server.addListener("request", handleRequest);
+   server.addListener("listening", handleListen);
+   server.listen(_port);
+ }
+ 
+ async function connectToDatabase(_url: string): Promise<void> {
+   let options: Mongo.MongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+   let mongoClient: Mongo.MongoClient = new Mongo.MongoClient(_url, options);
+   await mongoClient.connect();
+   locationCollection = mongoClient.db("Test").collection("Adventures");
+   console.log("Database connection from adventures: ", locationCollection != undefined);
+ 
+ }
+ 
+ 
+ function handleListen(): void {
+   console.log("Listening");
+ }
+ async function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): Promise<void> {
+   let q: url.UrlWithParsedQuery = url.parse(_request.url, true);
+   let daten: ParsedUrlQuery = q.query;
+   if (q.pathname == "//saveLocation") {
+     _response.write(await saveLocation(q.query, daten.lat, daten.long));
+   }
+   _response.end();
+ }*/
+/*async function saveLocation(_rückgabe: Input, _lat: string | string[], _long: string | string[]): Promise<string> {
+  let data: locations[] = await locationCollection.find().toArray();
+  if (_lat.toString().match("[a-zA-Z]+")&& _long.toString().match("[a-zA-Z]+")) {
+    return ("die location enthält buchstaben darf aber nur aus nummern bestehen");
+  }
+  
+  if (data.length > 0) {
+    for (let counter: number = 0; counter < data.length; counter++) {
+      if (data[counter].lat == Number(_lat)&& data[counter].long == Number(_long)) {
+        return "ein eintrag mit diesen Koordinaten besteht bereits";
+      }
+    }
+  }
+  locationCollection.insertOne(_rückgabe);
+  return "Location erfolgreich gespeichert";
+}
+*/
 var Prototyp;
 (function (Prototyp) {
-    let locationCollection;
-    let showCurrentCoordinates = "";
-    let iBaulat = 48.04994174315437;
-    let iBauLong = 8.210831942378386;
+    const startButton = document.getElementById("startButton");
+    startButton.addEventListener("click", startGame);
     let musicPlaying = false;
+    let currentsound = "";
+    let lastLocation = "";
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    let audioContext;
+    let audioBufferMap = new Map();
+    let audioSourceMap = new Map();
     const currentCoordinates = document.getElementById("currentCoordinates");
     const options = {
         enableHighAccuracy: true,
         maximumAge: 0,
     };
-    const startButton = document.getElementById("startButton");
-    startButton.addEventListener("click", buttonPressed);
-    function buttonPressed() {
+    class Location {
+        locationID;
+        lat;
+        long;
+        sound;
+        constructor(_locationID, _lat, _long, _sound) {
+            this.locationID = _locationID;
+            this.lat = +_lat;
+            this.long = +_long;
+            this.sound = _sound;
+        }
+    }
+    let locationMap = new Map();
+    async function startGame() {
         startButton.classList.add("hidden");
+        locationMap = await loadLocationMap();
+        audioContext = new AudioContext();
+        for (let [key, value] of locationMap) {
+            loadSound(value.sound);
+        }
         if ("geolocation" in navigator) {
             /* geolocation is available */
             navigator.geolocation.watchPosition(success, error, options);
@@ -28,68 +113,29 @@ var Prototyp;
             currentCoordinates.textContent = "coordinates not available";
         }
     }
-    let databaseUrl = "mongodb+srv://FynnJ:nicnjX5MjRSm4wtu@gis-ist-geil.wb5k5.mongodb.net/?retryWrites=true&w=majority";
-    console.log("Starting server");
-    let port = Number(process.env.PORT);
-    if (!port)
-        port = 8100;
-    startServer(port);
-    connectToDatabase(databaseUrl);
-    function startServer(_port) {
-        let server = Http.createServer();
-        server.addListener("request", handleRequest);
-        server.addListener("listening", handleListen);
-        server.listen(_port);
-    }
-    async function connectToDatabase(_url) {
-        let options = { useNewUrlParser: true, useUnifiedTopology: true };
-        let mongoClient = new Mongo.MongoClient(_url, options);
-        await mongoClient.connect();
-        locationCollection = mongoClient.db("Test").collection("Adventures");
-        console.log("Database connection from adventures: ", locationCollection != undefined);
-    }
-    function handleListen() {
-        console.log("Listening");
-    }
-    async function handleRequest(_request, _response) {
-        let q = url.parse(_request.url, true);
-        let daten = q.query;
-        if (q.pathname == "//saveLocation") {
-            _response.write(await saveLocation(q.query, daten.lat, daten.long));
-        }
-        _response.end();
-    }
-    async function saveLocation(_rückgabe, _lat, _long) {
-        let data = await locationCollection.find().toArray();
-        if (_lat.toString().match("[a-zA-Z]+") && _long.toString().match("[a-zA-Z]+")) {
-            return ("die location enthält buchstaben darf aber nur aus nummern bestehen");
-        }
-        if (data.length > 0) {
-            for (let counter = 0; counter < data.length; counter++) {
-                if (data[counter].lat == Number(_lat) && data[counter].long == Number(_long)) {
-                    return "ein eintrag mit diesen Koordinaten besteht bereits";
-                }
-            }
-        }
-        locationCollection.insertOne(_rückgabe);
-        return "Location erfolgreich gespeichert";
-    }
     function success(_pos) {
-        showCurrentCoordinates = "Latitude: " + _pos.coords.latitude + ", Longitude: " + _pos.coords.longitude + ", genauigkeit :" + _pos.coords.accuracy;
-        currentCoordinates.textContent = showCurrentCoordinates;
-        loadSound("../interactive_Audio/audio/iBau.mp3");
+        //  currentCoordinates.textContent = "" + _pos.coords.latitude +", "+  _pos.coords.longitude;
+        currentCoordinates.textContent = "distanz zum ziel" + checkDistanceBetween(_pos, 47.579136, 7.6218368);
         checkForLocations(_pos);
     }
     function checkForLocations(_currentCoordinates) {
-        let d = checkDistanceBetween(_currentCoordinates, iBaulat, iBauLong);
-        currentCoordinates.textContent = currentCoordinates.textContent + " distanz zum i bau =" + d;
-        if (d < 0.05) {
-            playSound("../interactive_Audio/audio/iBau.mp3", true, 1000);
-            musicPlaying = true;
-        }
-        if (d > 0.05 && musicPlaying) {
-            stopSound("../interactive_Audio/audio/iBau.mp3");
-            musicPlaying = false;
+        for (let [key, value] of locationMap) {
+            let d = checkDistanceBetween(_currentCoordinates, value.lat, value.long);
+            if (!musicPlaying) {
+                if (d < 0.05) {
+                    playSound(value.sound, true, 1000);
+                    musicPlaying = true;
+                    currentsound = value.sound;
+                    lastLocation = value.locationID;
+                    currentCoordinates.textContent = " du befindest dich in der nähe von: " + value.locationID + " deshalb hörst du etwas.";
+                    break;
+                }
+            }
+            if (musicPlaying && value.locationID === lastLocation && d > 0.05) {
+                console.log("ich brech ab");
+                stopSound(currentsound);
+                musicPlaying = false;
+            }
         }
     }
     function checkDistanceBetween(_pos, _lat, _long) {
@@ -107,10 +153,6 @@ var Prototyp;
     function error() {
         alert("error");
     }
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    const audioContext = new AudioContext();
-    let audioBufferMap = new Map();
-    let audioSourceMap = new Map();
     async function loadSound(_url) {
         let response = await fetch(_url);
         let arraybuffer = await response.arrayBuffer();
@@ -126,8 +168,23 @@ var Prototyp;
         audioSourceMap.set(_url, source);
     }
     function stopSound(_url) {
+        console.log("ich stooppe");
         audioSourceMap.get(_url).stop();
         audioSourceMap.delete(_url);
+    }
+    async function loadJson(_path) {
+        let response = await fetch(_path);
+        let json = await response.json();
+        return json;
+    }
+    async function loadLocationMap() {
+        let arrayObject = await loadJson("../interactive_Audio/data/locations.json");
+        let tempLocationMap = new Map();
+        for (let i = 0; i < arrayObject.length; i++) {
+            let loc = new Location(arrayObject[i].locationID, arrayObject[i].lat, arrayObject[i].long, arrayObject[i].sound);
+            tempLocationMap.set(loc.locationID, loc);
+        }
+        return tempLocationMap;
     }
 })(Prototyp || (Prototyp = {}));
 //# sourceMappingURL=script.js.map
--- interactive_Audio/script.ts
+++ interactive_Audio/script.ts
@@ -1,38 +1,123 @@
-import * as Http from "http";
+/*import * as Http from "http";
 import { ParsedUrlQuery } from "querystring";
 import * as url from "url";
-import * as Mongo from "mongodb";
+import * as Mongo from "mongodb";*/
+/*let locationCollection: Mongo.Collection;
+ 
+ 
 
+interface Input {
+  [type: string]: string | string[];
+}
+*/
+/*
+ let databaseUrl: string = "mongodb+srv://FynnJ:nicnjX5MjRSm4wtu@gis-ist-geil.wb5k5.mongodb.net/?retryWrites=true&w=majority";
+ console.log("Starting server");
+ let port: number = Number(process.env.PORT);
+ if (!port)
+   port = 8100;
+ 
+ startServer(port);
+ connectToDatabase(databaseUrl);
+ 
+ 
+ 
+ function startServer(_port: number | string): void {
+   let server: Http.Server = Http.createServer();
+   server.addListener("request", handleRequest);
+   server.addListener("listening", handleListen);
+   server.listen(_port);
+ }
+ 
+ async function connectToDatabase(_url: string): Promise<void> {
+   let options: Mongo.MongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+   let mongoClient: Mongo.MongoClient = new Mongo.MongoClient(_url, options);
+   await mongoClient.connect();
+   locationCollection = mongoClient.db("Test").collection("Adventures");
+   console.log("Database connection from adventures: ", locationCollection != undefined);
+ 
+ }
+ 
+ 
+ function handleListen(): void {
+   console.log("Listening");
+ }
+ async function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): Promise<void> {
+   let q: url.UrlWithParsedQuery = url.parse(_request.url, true);
+   let daten: ParsedUrlQuery = q.query;
+   if (q.pathname == "//saveLocation") {
+     _response.write(await saveLocation(q.query, daten.lat, daten.long));
+   }
+   _response.end();
+ }*/
+/*async function saveLocation(_rückgabe: Input, _lat: string | string[], _long: string | string[]): Promise<string> {
+  let data: locations[] = await locationCollection.find().toArray();
+  if (_lat.toString().match("[a-zA-Z]+")&& _long.toString().match("[a-zA-Z]+")) {
+    return ("die location enthält buchstaben darf aber nur aus nummern bestehen");
+  }
+  
+  if (data.length > 0) {
+    for (let counter: number = 0; counter < data.length; counter++) {
+      if (data[counter].lat == Number(_lat)&& data[counter].long == Number(_long)) {
+        return "ein eintrag mit diesen Koordinaten besteht bereits";
+      }
+    }
+  }
+  locationCollection.insertOne(_rückgabe);
+  return "Location erfolgreich gespeichert";
+}
+*/
 namespace Prototyp {
 
+  const startButton = document.getElementById("startButton");
+  startButton.addEventListener("click", startGame);
 
-  let locationCollection: Mongo.Collection;
-  let showCurrentCoordinates: string = "";
-  let iBaulat: number = 48.04994174315437;
-  let iBauLong: number = 8.210831942378386;
+
+
+
   let musicPlaying: boolean = false;
+  let currentsound: string = "";
+  let lastLocation: string = "";
 
-  interface Input {
-    [type: string]: string | string[];
-  }
 
-  interface locations {
-    lat:number;
-    long:number;
-    sound:number;
-}
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  let audioContext: AudioContext;
+  let audioBufferMap: Map<string, AudioBuffer> = new Map();
+  let audioSourceMap: Map<string, AudioBufferSourceNode> = new Map();
+
   const currentCoordinates: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById("currentCoordinates");
-
   const options = {
     enableHighAccuracy: true,
     maximumAge: 0,
   };
+  class Location {
+    locationID: string;
+    lat: number;
+    long: number;
+    sound: string;
+    constructor(_locationID: string, _lat: string, _long: string, _sound: string) {
+      this.locationID = _locationID;
+      this.lat = +_lat;
+      this.long = +_long;
+      this.sound = _sound;
+    }
+  }
+  interface Serialized {
+    [key: string]: any;
+  }
+  let locationMap: Map<string, Location> = new Map();
 
-  const startButton = document.getElementById("startButton");
-  startButton.addEventListener("click", buttonPressed);
 
-  function buttonPressed(): void {
+
+
+  async function startGame(): Promise<void> {
     startButton.classList.add("hidden");
+    locationMap = await loadLocationMap();
+
+    audioContext = new AudioContext();
+    for (let [key, value] of locationMap) {
+      loadSound(value.sound);
+    }
     if ("geolocation" in navigator) {
       /* geolocation is available */
       navigator.geolocation.watchPosition(success, error, options);
@@ -43,82 +128,36 @@ namespace Prototyp {
     }
   }
 
-  let databaseUrl: string = "mongodb+srv://FynnJ:nicnjX5MjRSm4wtu@gis-ist-geil.wb5k5.mongodb.net/?retryWrites=true&w=majority";
-  console.log("Starting server");
-  let port: number = Number(process.env.PORT);
-  if (!port)
-    port = 8100;
-
-  startServer(port);
-  connectToDatabase(databaseUrl);
-
-
-
-  function startServer(_port: number | string): void {
-    let server: Http.Server = Http.createServer();
-    server.addListener("request", handleRequest);
-    server.addListener("listening", handleListen);
-    server.listen(_port);
-  }
-
-  async function connectToDatabase(_url: string): Promise<void> {
-    let options: Mongo.MongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
-    let mongoClient: Mongo.MongoClient = new Mongo.MongoClient(_url, options);
-    await mongoClient.connect();
-    locationCollection = mongoClient.db("Test").collection("Adventures");
-    console.log("Database connection from adventures: ", locationCollection != undefined);
-
-  }
-
-
-  function handleListen(): void {
-    console.log("Listening");
-  }
-  async function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): Promise<void> {
-    let q: url.UrlWithParsedQuery = url.parse(_request.url, true);
-    let daten: ParsedUrlQuery = q.query;
-    if (q.pathname == "//saveLocation") {
-      _response.write(await saveLocation(q.query, daten.lat, daten.long));
-    }
-    _response.end();
-  }
-  async function saveLocation(_rückgabe: Input, _lat: string | string[], _long: string | string[]): Promise<string> {
-    let data: locations[] = await locationCollection.find().toArray();
-    if (_lat.toString().match("[a-zA-Z]+")&& _long.toString().match("[a-zA-Z]+")) {
-      return ("die location enthält buchstaben darf aber nur aus nummern bestehen");
-    }
-    
-    if (data.length > 0) {
-      for (let counter: number = 0; counter < data.length; counter++) {
-        if (data[counter].lat == Number(_lat)&& data[counter].long == Number(_long)) {
-          return "ein eintrag mit diesen Koordinaten besteht bereits";
-        }
-      }
-    }
-    locationCollection.insertOne(_rückgabe);
-    return "Location erfolgreich gespeichert";
-  }
 
   function success(_pos: GeolocationPosition): void {
-    showCurrentCoordinates = "Latitude: " + _pos.coords.latitude + ", Longitude: " + _pos.coords.longitude + ", genauigkeit :" + _pos.coords.accuracy;
-    currentCoordinates.textContent = showCurrentCoordinates;
-    loadSound("../interactive_Audio/audio/iBau.mp3");
-    checkForLocations(_pos);
+   //  currentCoordinates.textContent = "" + _pos.coords.latitude +", "+  _pos.coords.longitude;
+  currentCoordinates.textContent = "distanz zum ziel" + checkDistanceBetween(_pos,47.579136, 7.6218368);
+   checkForLocations(_pos);
   }
 
   function checkForLocations(_currentCoordinates: GeolocationPosition): void {
-    let d: number = checkDistanceBetween(_currentCoordinates, iBaulat, iBauLong);
-    currentCoordinates.textContent = currentCoordinates.textContent + " distanz zum i bau =" + d;
+    for (let [key, value] of locationMap) {
+      let d: number = checkDistanceBetween(_currentCoordinates, value.lat, value.long);
+      if (!musicPlaying) {
+        if (d < 0.05) {
+          
+          playSound(value.sound, true,1000);
+          musicPlaying = true;
+          currentsound = value.sound;
+          lastLocation = value.locationID;
+          currentCoordinates.textContent = " du befindest dich in der nähe von: " + value.locationID + " deshalb hörst du etwas."
 
-    if (d < 0.05) {
-      playSound("../interactive_Audio/audio/iBau.mp3", true, 1000);
-      musicPlaying = true;
-    }
-    if (d > 0.05 && musicPlaying) {
-      stopSound("../interactive_Audio/audio/iBau.mp3");
-      musicPlaying = false;
+          break;
+        }
+      }
+      if (musicPlaying && value.locationID === lastLocation && d > 0.05) {
+        console.log("ich brech ab");
+        stopSound(currentsound);
+        musicPlaying = false;
+      }
     }
   }
+
   function checkDistanceBetween(_pos: GeolocationPosition, _lat: number, _long: number) {
     let R: number = 6371; // Radius of the earth in km
     let dLat: number = deg2rad(_lat - _pos.coords.latitude);  // deg2rad below
@@ -137,10 +176,9 @@ namespace Prototyp {
     alert("error");
   }
 
-  window.AudioContext = window.AudioContext || window.webkitAudioContext;
-  const audioContext: AudioContext = new AudioContext();
-  let audioBufferMap: Map<string, AudioBuffer> = new Map();
-  let audioSourceMap: Map<string, AudioBufferSourceNode> = new Map();
+
+
+
 
 
   async function loadSound(_url: string): Promise<void> {
@@ -160,7 +198,30 @@ namespace Prototyp {
   }
 
   function stopSound(_url: string): void {
+    console.log("ich stooppe")
     audioSourceMap.get(_url).stop();
     audioSourceMap.delete(_url);
   }
+
+
+
+  async function loadJson(_path: string): Promise<JSON> {
+    let response: Response = await fetch(_path);
+    let json: JSON = await response.json();
+    return json;
+  }
+  async function loadLocationMap(): Promise<Map<string, Location>> {
+    let arrayObject: Serialized = await loadJson("../interactive_Audio/data/locations.json");
+
+    let tempLocationMap: Map<string, Location> = new Map();
+
+    for (let i: number = 0; i < arrayObject.length; i++) {
+      let loc: Location = new Location(arrayObject[i].locationID, arrayObject[i].lat, arrayObject[i].long, arrayObject[i].sound);
+
+      tempLocationMap.set(loc.locationID, loc);
+    }
+    return tempLocationMap;
+  }
+
 }
+
